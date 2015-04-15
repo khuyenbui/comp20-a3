4ee11c1da205d9158959e659225d81f05f78d4e3
Khuyen <khuyen.bui@tufts.edu>
2015-04-15 14:26:05 -0400

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+// 5000 is the gate.
+
 // Express initialization
 var express = require('express');
+var bodyParser = require('body-parser');
+var validator = require('validator'); // See documentation at https://github.com/chriso/validator.js
 var app = express();
 
 // set Cross-Origin Policy
@@ -12,56 +16,94 @@ app.all('/', function(req, res, next) {
 // Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost/whereintheworld'; // comp20 is the name of the database we are using in MongoDB
-var mongo = require('mongodb');
-var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+  'mongodb://localhost/test'; // comp20 is the name of the database we are using in MongoDB
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
   db = databaseConnection;
 });
 
 // API for POST.
 // how to send the error messages?
+// LOG: Need to check the insert function again.
 
-app.post('/sendLocation', function(request, response) { // whut is this 1st arg?
-        var lat = request.lat;
-        var lng = request.lng;
-        var login = request.login;
-        if (lat == NULL || lng == NULL || login == NULL) {
+app.post('/sendLocation', function (request, response) { // whut is this 1st arg?
+        var newLat = parseFloat(request.body.lat);
+        var newLng = parseFloat(request.body.lng);
+        var newLogin = request.body.login;
+        if ('login' in request.body.login) {
                 response.send('{"error":"Whoops, something is wrong with your data!"}');
         }
-        var timeStamp = request.Timestamp;
-        var toInsert = {
-                "location": lat, lng, login, timeStamp,
-        };
-        // make sure it's not repeated.
-        db.locations.update(
-                {login: login},
-                {
-                        login: "login",
-                        lat: lat,
-                        lng: lng,
-                }
-                { upsert: true }
-                )
-        /*
+        var timeStamp = request.getDate();
+        // use update with a call back
+        // then I need to return everything.
         db.collection('locations', function(er, collection) {
-                if (db.collection.update(login))
-                var id = collection.insert(toInsert, function(err, saved) {
-                        if (err) {
-                                response.send(500);
-                        }
-                        else {
+                db.locations.update(
+                        { login: newLogin },
+                        {
+                                login: newLogin,
+                                lat: newLat,
+                                lng: newLng,
+                                created_at: timeStamp
+                        },
+                        { upsert: true },
+                        function(err, result) {
+                          // depending, we need to send back the result
+                          // response.send({"error": "some messages"});
+                          if (err) {
+                                response.status(500);
+                                response.send("Whoop something went wrong");
+                          }
+                          else {
                                 response.send(200);
-                        }
-            });
+                                // how do I send back the result as array? db.find()
+                                result = json(db.locations.find().toArray());
+                                response.send(result);
+                          }
+                        });
         });
-        */
 });
 
+// return a JSON
+app.get('/location.json', function (request, response) {
+  db.collection('locations', function (err, collection) {
+/*    var login = request.query.login;
+    // if login name isn't there, send back empty json.
+    var myLogin = db.locations.findOne(login).toArray(function (err, result)
+      {
+
+      });
+    if (login == NULL) {
+      response.send('{}');
+    }
+    else {
+      response.send(json())  // TODO: send back the object here.
+    }
+  }); */
+});
+
+// the home directory.
 app.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
-  response.send('<p>Hey, it works!</p>');
+  var indexPage = '';
+  db.collection('locations', function(er, collection) {
+    collection.find().toArray(function(err, cursor) {
+        if (!err) {
+          indexPage += "<!DOCTYPE HTML><html><head><title>Locations</title></head><body><h1>Wonder everyone is?</h1>";
+          for (var count = 0; count < cursor.length; count++) {
+            var login = cursor[count].login;
+            var lat = cursor[count].lat;
+            var lng = cursor[count].lng;
+            var time = cursor[count].created_at;
+            indexPage += "<p>" + login + " checked in at " + lat + " , " + lng + "at time " + time + " </p>";
+          }
+        indexPage += "</body></html>"
+        response.send(indexPage);
+        response.send('<p>whut is going on</p>');
+      }
+    });
+  });
 });
 
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
